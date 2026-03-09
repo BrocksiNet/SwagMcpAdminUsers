20d4c193dc1c54c85508e57f59cebe8c0d7c6587
BrocksiNet <b.meyer@shopware.com>
2026-03-09 14:44:58 +0100

    Implement conditional build method in SwagMcpAdminUsers to check for MCP_SERVER feature availability.

--- src/SwagMcpAdminUsers.php
+++ src/SwagMcpAdminUsers.php
@@ -2,8 +2,18 @@
 
 namespace Swag\McpAdminUsers;
 
+use Shopware\Core\Framework\Feature;
 use Shopware\Core\Framework\Plugin;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class SwagMcpAdminUsers extends Plugin
 {
+    public function build(ContainerBuilder $container): void
+    {
+        if (!Feature::has('MCP_SERVER')) {
+            return;
+        }
+
+        parent::build($container);
+    }
 }
